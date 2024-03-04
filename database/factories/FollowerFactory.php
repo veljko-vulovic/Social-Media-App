2c02cf988c4c449bd7f9bfb7e640cb79712d9320
Veljko <vulovic.veljko97@gmail.com>
2024-03-04 01:00:14 +0100

    Fix followers factory

--- database/factories/FollowerFactory.php
+++ database/factories/FollowerFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\Follower;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
@@ -17,12 +18,21 @@ class FollowerFactory extends Factory
      */
     public function definition(): array
     {
-        $user_id = User::find(1)->first()->id;
-        $follower_id = User::inRandomOrder()->first()->id;
+        // Get existing user IDs
+        $userIds = User::pluck('id')->toArray();
+
+        $followerId = $this->faker->randomElement($userIds);
+        $followingId = $this->faker->randomElement($userIds);
+
+        // Ensure unique combination of follower_id and following_id
+        while (Follower::where('follower_id', $followerId)->where('following_id', $followingId)->exists()) {
+            $followerId = $this->faker->randomElement($userIds);
+            $followingId = $this->faker->randomElement($userIds);
+        }
 
         return [
-            'follower_id' => $user_id,
-            'following_id' => $follower_id,
+            'follower_id' => $followerId,
+            'following_id' => $followingId,
         ];
     }
 }
